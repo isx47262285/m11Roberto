f5abea48024b62e7d92a7dc78cbc5e6b80d1e6b0
root <root@ip-172-31-22-139.eu-west-2.compute.internal>
2019-05-03 10:40:46 +0000

    act readme

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ echo "pere" | passwd --stdin pere
 echo "marta" | passwd --stdin marta
 echo "@edt ASIX M11-SAD" > /var/www/index.html
 /usr/bin/ssh-keygen -A
-cp /opt/docker/xinetd.d/* /etc/xinetd.d/
-cp /opt/docker/marta/ /var/spool/mail/marta
+cp /opt/docker/marta /var/spool/mail/marta
+chown marta.mail /var/spool/mail/marta
 cp /opt/docker/pere/ /var/spool/mail/pere
+chown pere.mail /var/spool/mail/marta
+cp /opt/docker/xinetd.d/* /etc/xinetd.d/
 
